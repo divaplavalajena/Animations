2a84382a1b8912d8ffbeda5753171894031042ec
Jena Grafton <JenaLGrafton@gmail.com>
2015-10-14 18:48:03 -0500

    Added code for start and stop of animation

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -14,7 +14,39 @@ class ViewController: UIViewController {
     
     var counter = 1
     
+    var timer = NSTimer()
+    
+    var isAnimating = true
+    
     @IBAction func updateImage(sender: AnyObject) {
+        
+        if isAnimating == true {
+            
+            timer.invalidate()
+            
+            isAnimating = false
+            
+        } else {
+            
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+            
+            isAnimating = true
+            
+        }
+        
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: Selector("doAnimation"), userInfo: nil, repeats: true)
+        
+    }
+    
+    func doAnimation() {
         
         if counter == 5 {
             
@@ -27,24 +59,23 @@ class ViewController: UIViewController {
         }
         
         alienImage.image = UIImage(named: "frame\(counter).png")
+
         
     }
     
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    /*
+    
     override func viewDidLayoutSubviews() {
         
-        alienImage.alpha = 0
+        alienImage.frame =  CGRectMake(100, 20, 0, 0)
         
         
     }
@@ -53,13 +84,14 @@ class ViewController: UIViewController {
         
         UIView.animateWithDuration(1, animations: { () -> Void in
             
-            self.alienImage.alpha = 1
+            self.alienImage.frame = CGRectMake(100, 20, 100, 200)
+
             
         })
         
         
     }
-
+*/
 
 }
 
